cfa35645779d0046ebedec076ba32d0dc7e4c384
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2026-04-23 18:40:27 +0200

    feat: Implement AbstractEntity class

---
+++ src/core/AbstractEntity.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Core;
+
+abstract class AbstractEntity
+{
+    protected ?int $id = null;
+
+    public function __construct(array $data = [])
+    {
+        if (!empty($data)) {
+            $this->hydrate($data);
+        }
+    }
+
+    protected function hydrate(array $data): void
+    {
+        foreach ($data as $key => $value) {
+            // Transformation de user_id en setUserId
+            $method = 'set' . str_replace('_', '', ucwords($key, '_'));
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+}
