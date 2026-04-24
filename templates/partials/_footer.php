83528ec1272d102d05e69c63b0281c33f6ddcc5f
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2026-04-24 18:30:15 +0200

    feat: Add footer section with privacy policy and legal mentions, including logo

--- templates/partials/_footer.php
+++ templates/partials/_footer.php
@@ -1,5 +1,18 @@
-<footer class="bg-dark text-white py-4 mt-auto">
-    <div class="container text-center">
-        <p class="mb-0">&copy; 2026 TomTroc - Échangez vos livres !</p>
-    </div>
-</footer>
+<div class="container">
+    <footer class="row py-5 my-5 border-top">
+        <div class="col-md-2 mb-3 ms-auto text-end">
+            <h5 class="fw-normal small">Politique de confidentialité</h5>
+        </div>
+        <div class="col-md-2 mb-3 text-end">
+            <h5 class="fw-normal small">Mentions légales</h5>
+        </div>
+        <div class="col-md-1 mb-3 text-end">
+            <h5 class="fw-normal small">Tom Troc©</h5>
+        </div>
+        <div class="col-md-1 mb-3 text-end">
+            <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none" aria-label="Bootstrap">
+                <img src="/assets/icons/logo_footer.svg" alt="TomTroc" class="footer-logo d-inline-block align-text-top">
+            </a>
+        </div>
+    </footer>
+</div>
