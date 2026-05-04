289cb6aca716220d422dcb28b9fc74806d41ccc9
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2026-05-04 20:38:29 +0200

    style: Refactor footer layout for improved design and spacing

--- templates/partials/_footer.php
+++ templates/partials/_footer.php
@@ -1,18 +1,18 @@
-<div class="container">
-    <footer class="row py-5 my-5 border-top">
-        <div class="col-md-2 mb-3 ms-auto text-end">
+<footer class="bg-white py-1">
+    <div class="row d-flex align-items-center">
+        <div class="col-md-2 mb-1 ms-auto text-end">
             <h5 class="fw-normal small">Politique de confidentialité</h5>
         </div>
-        <div class="col-md-2 mb-3 text-end">
+        <div class="col-md-2 mb-1 text-end">
             <h5 class="fw-normal small">Mentions légales</h5>
         </div>
-        <div class="col-md-1 mb-3 text-end">
+        <div class="col-md-1 mb-1 text-end">
             <h5 class="fw-normal small">Tom Troc©</h5>
         </div>
-        <div class="col-md-1 mb-3 text-end">
+        <div class="col-md-1 mb-1 text-end">
             <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none" aria-label="Bootstrap">
                 <img src="/assets/icons/logo_footer.svg" alt="TomTroc" class="footer-logo d-inline-block align-text-top">
             </a>
         </div>
-    </footer>
-</div>
+    </div>
+</footer>
