56f5d21e902be5a477782075fad505e41980a074
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2026-05-26 22:04:15 +0200

    feat: Update layout styles for improved responsiveness and structure

--- templates/layout.php
+++ templates/layout.php
@@ -7,13 +7,14 @@
     <title><?= $title ?? 'TomTroc' ?></title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 
-<body>
+<body class="d-flex flex-column min-vh-100">
     <?php include 'partials/_header.php'; ?>
 
-    <main>
+    <main class="flex-grow-1 d-flex flex-column">
         <?= $content ?>
     </main>
 
