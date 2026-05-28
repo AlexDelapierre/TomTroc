c3369891fe7db625bd07ce081b08cbe7fdbee2d2
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2026-05-28 20:12:05 +0200

    fix: Improve layout and styling in home section for better responsiveness and readability

--- templates/home.php
+++ templates/home.php
@@ -1,9 +1,9 @@
 <section>
-    <div class="container container-sm py-5">
+    <div class="container py-5">
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-            <div class="col-10 col-sm-8 col-lg-6">
+            <div class="col-12 col-lg-6">
                 <img src="assets/img/hero-image.jpg"
-                    class="d-block mx-lg-auto img-fluid"
+                    class="d-block mx-lg-auto img-fluid img-mobile-full"
                     alt="Bootstrap Themes" width="700" height="500" loading="lazy">
             </div>
             <div class="col-lg-6">
@@ -23,7 +23,7 @@
 
         <ul class="row list-unstyled">
             <?php foreach ($books as $book): ?>
-                <li class="col-12 col-md-6 col-lg-3 mb-4">
+                <li class="col-6 col-md-6 col-lg-3 mb-4">
                     <?php include 'partials/_book_card.php'; ?>
                 </li>
             <?php endforeach; ?>
@@ -54,7 +54,7 @@
             foreach ($steps as $step):
             ?>
                 <li class="col-12 col-md-6 col-lg-3 mb-4">
-                    <article class="card h-100 bg-white p-3 border-0 rounded-0">
+                    <article class="card h-100 bg-white p-5 p-md-3 border-0 rounded-0 step-card align-items-center justify-content-center text-center">
                         <p class="mb-0 text-break">
                             <?= $step ?>
                         </p>
@@ -78,11 +78,9 @@
         <div class="row justify-content-center">
             <div class="class=col-md-8 col-lg-4">
                 <h2 class="text-center mb-4">Nos valeurs</h2>
-                <p>
-                    Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour rassembler les gens et inspirer des conversations enrichissantes.<br>
-                    Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé.<br>
-                    Nous sommes passionnés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.
-                </p>
+                <p>Chez Tom Troc, nous mettons l'accent sur le partage, la découverte et la communauté. Nos valeurs sont ancrées dans notre passion pour les livres et notre désir de créer des liens entre les lecteurs. Nous croyons en la puissance des histoires pour rassembler les gens et inspirer des conversations enrichissantes.</p>
+                <p>Notre association a été fondée avec une conviction profonde : chaque livre mérite d'être lu et partagé.</p>
+                <p> Nous sommes passionnés par la création d'une plateforme conviviale qui permet aux lecteurs de se connecter, de partager leurs découvertes littéraires et d'échanger des livres qui attendent patiemment sur les étagères.</p>
                 <div class="d-flex align-items-center gap-3 mt-4">
                     <p>L’équipe Tom Troc</p>
                     <img src="/assets/icons/Vector2.svg" alt="TomTroc" class="navbar-logo d-inline-block align-text-top">
