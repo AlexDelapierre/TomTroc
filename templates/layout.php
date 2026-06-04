c2ab7f72ec4c7b051fed4456a9684393d5d70ae3
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2026-06-04 22:34:55 +0200

    refactor(layout): wrap page content in a central container

--- templates/layout.php
+++ templates/layout.php
@@ -12,17 +12,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="/assets/css/style.css">
-    <link rel="stylesheet" href="/assets/css/messages.css">
 </head>
 
-<body class="d-flex flex-column min-vh-100">
-    <?php include 'partials/_header.php'; ?>
+<body class="d-flex flex-column">
+    <div class="container flex-grow-1 d-flex flex-column layout-wrapper">
+        <?php include 'partials/_header.php'; ?>
 
-    <main class="flex-grow-1 d-flex flex-column">
-        <?= $content ?>
-    </main>
+        <main class="flex-grow-1 d-flex flex-column">
+            <?= $content ?>
+        </main>
 
-    <?php include 'partials/_footer.php'; ?>
+        <?php include 'partials/_footer.php'; ?>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
