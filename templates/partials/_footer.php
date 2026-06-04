7df20f3868035b071d0aba2601a32cfdf27005e3
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2026-06-04 22:36:06 +0200

    style(footer): adjust layout alignment and text styling

--- templates/partials/_footer.php
+++ templates/partials/_footer.php
@@ -1,17 +1,17 @@
-<footer class="bg-white py-1">
-    <div class="row d-flex align-items-center">
-        <div class="col-md-2 mb-1 ms-auto text-end">
-            <h5 class="fw-normal small">Politique de confidentialité</h5>
+<footer class="bg-white py-3 px-5">
+    <div class="row d-flex align-items-center footer-row justify-content-end">
+        <div class="col-auto text-end">
+            <h5 class="fw-normal text-xss text-dark m-0">Politique de confidentialité</h5>
         </div>
-        <div class="col-md-2 mb-1 text-end">
-            <h5 class="fw-normal small">Mentions légales</h5>
+        <div class="col-auto text-end">
+            <h5 class="fw-normal text-xss text-dark m-0">Mentions légales</h5>
         </div>
-        <div class="col-md-1 mb-1 text-end">
-            <h5 class="fw-normal small">Tom Troc©</h5>
+        <div class="col-auto text-end">
+            <h5 class="fw-normal text-xss text-dark m-0">Tom Troc©</h5>
         </div>
-        <div class="col-md-1 mb-1 text-end">
-            <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none" aria-label="Bootstrap">
-                <img src="/assets/icons/logo_footer.svg" alt="TomTroc" class="footer-logo d-inline-block align-text-top">
+        <div class="col-auto text-end pe-2">
+            <a href="/" class="d-flex align-items-center link-body-emphasis text-decoration-none" aria-label="Bootstrap">
+                <img src="/assets/icons/logo_footer.svg" alt="TomTroc" class="footer-logo d-inline-block ms-2">
             </a>
         </div>
     </div>
